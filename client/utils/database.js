3b822f5834afccf8d330f04275a2d66a8cc5c3d3
EzekielBrown <zek.bro@hotmail.com>
2023-05-19 21:02:05 +1200

    updated

--- client/utils/database.js
+++ client/utils/database.js
@@ -13,7 +13,7 @@ export const connectToDB = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
+            dbName: "Hands-For-Hope",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
